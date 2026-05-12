4d48698c5cae21396d60492ca796cef4230ca95e
Bùi Thanh Sang <23130268@st.hcmuaf.edu.vn>
2026-05-13 00:00:47 +0700

    completefor affine cipher

--- src/main/java/vn/edu/hcmuaf/fit/View/ViewAlgrorithmClassical/AffinePanel.java
+++ src/main/java/vn/edu/hcmuaf/fit/View/ViewAlgrorithmClassical/AffinePanel.java
@@ -2,94 +2,124 @@ package vn.edu.hcmuaf.fit.View.ViewAlgrorithmClassical;
 
 import javax.swing.*;
 import java.awt.*;
-
 public class AffinePanel extends JPanel {
-    private JTextArea input,output;
-    private JTextField key;
-    private JButton encryptButton, decryptButton, loadFileButton, clearButton, genKeyButton;
+    private JTextArea input, output;
+    private JTextField keyA, keyB;
+    private JButton encryptButton, decryptButton,loadFileButton, clearButton,genKeyButton, importKeyButton, exportKeyButton;
     private JLabel title;
-    private JScrollPane inputScroll, outputScroll;
 
     public AffinePanel() {
-        setLayout(new BorderLayout(10,10));
-        // title
+        setLayout(new BorderLayout(10, 10));
+
+       // process for title affine
         title = new JLabel("AFFINE CIPHER", JLabel.CENTER);
         title.setFont(new Font("Arial", Font.BOLD, 18));
-        add(title,BorderLayout.NORTH);
-        //content include input
-        JPanel centerPanel = new JPanel(new GridLayout(2,1,10,10));
+
+        add(title, BorderLayout.NORTH);
+
+        // process for content include input and output
+        JPanel centerPanel = new JPanel(new GridLayout(2, 1, 10, 10));
+
         input = new JTextArea();
         input.setLineWrap(true);
         input.setWrapStyleWord(false);
         input.setBorder(BorderFactory.createTitledBorder("Input"));
-        inputScroll = new JScrollPane(input);
-        inputScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
         output = new JTextArea();
         output.setLineWrap(true);
         output.setWrapStyleWord(false);
-        output.setBorder(BorderFactory.createTitledBorder("Output"));
-        // not edit but user can coppy-BuiThanhSang writting not AI please. all comment in this project author is bui thanh sang never AI
         output.setEditable(false);
-        outputScroll = new JScrollPane(output);
-        outputScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        output.setBorder(BorderFactory.createTitledBorder("Output"));
 
-        centerPanel.add(inputScroll);
-        centerPanel.add(outputScroll);
-        add(centerPanel,BorderLayout.CENTER);
+        centerPanel.add(new JScrollPane(input));
+        centerPanel.add(new JScrollPane(output));
 
-        // for all button in panel
-        JPanel bottomPanel = new JPanel(new BorderLayout(10,10));
+        add(centerPanel, BorderLayout.CENTER);
 
-        // pannel for show key and user can generate key
+        // process for button in panel
+        JPanel bottomPanel = new JPanel(new BorderLayout(10, 10));
+
+        // process for key
         JPanel keyPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        keyPanel.add(new JLabel("key:"));
-        key = new JTextField(10);
-        keyPanel.add(key);
+
+        keyPanel.add(new JLabel("Key A:"));
+        keyA = new JTextField(5);
+        keyPanel.add(keyA);
+
+        keyPanel.add(new JLabel("Key B:"));
+        keyB = new JTextField(5);
+        keyPanel.add(keyB);
+
         genKeyButton = new JButton("Generate Key");
+        importKeyButton = new JButton("Import Key");
+        exportKeyButton = new JButton("Export Key");
+
         keyPanel.add(genKeyButton);
+        keyPanel.add(importKeyButton);
+        keyPanel.add(exportKeyButton);
 
-        // pannel for button
-
+        // button choose encrypt or decrypt
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+
         encryptButton = new JButton("Encrypt");
         decryptButton = new JButton("Decrypt");
         loadFileButton = new JButton("Load File .txt");
         clearButton = new JButton("Clear");
+
         buttonPanel.add(encryptButton);
         buttonPanel.add(decryptButton);
         buttonPanel.add(loadFileButton);
         buttonPanel.add(clearButton);
 
-        bottomPanel.add(keyPanel,BorderLayout.NORTH);
-        bottomPanel.add(buttonPanel,BorderLayout.CENTER);
+        bottomPanel.add(keyPanel, BorderLayout.NORTH);
+        bottomPanel.add(buttonPanel, BorderLayout.CENTER);
 
-        add(bottomPanel,BorderLayout.SOUTH);
+        add(bottomPanel, BorderLayout.SOUTH);
     }
 
-    // process getter for controller call and set data to view
+
     public JTextArea getInput() {
         return input;
     }
+
     public JTextArea getOutput() {
         return output;
     }
-    public JTextField getKey() {
-        return key;
+
+
+    public JTextField getKeyA() {
+        return keyA;
     }
+
+    public JTextField getKeyB() {
+        return keyB;
+    }
+
     public JButton getEncryptButton() {
         return encryptButton;
     }
+
     public JButton getDecryptButton() {
         return decryptButton;
     }
+
     public JButton getLoadFileButton() {
         return loadFileButton;
     }
+
     public JButton getClearButton() {
         return clearButton;
     }
+
     public JButton getGenKeyButton() {
         return genKeyButton;
+    }
+
+    public JButton getImportKeyButton() {
+        return importKeyButton;
+    }
+
+    public JButton getExportKeyButton() {
+        return exportKeyButton;
     }
 }
--- src/main/java/vn/edu/hcmuaf/fit/model/Classicals/AffineCipher.java
+++ src/main/java/vn/edu/hcmuaf/fit/model/Classicals/AffineCipher.java
@@ -1,5 +1,227 @@
 package vn.edu.hcmuaf.fit.model.Classicals;
 
+import java.util.Random;
+
 public class AffineCipher{
+    private static final String EN_LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String EN_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private static final String VI_LOWER =
+            "aáàảãạâấầẩẫậăắằẳẵặ" +
+                    "bcdđ" +
+                    "eéèẻẽẹêếềểễệ" +
+                    "ghiíìỉĩị" +
+                    "klmnoóòỏõọôốồổỗộơớờởỡợ" +
+                    "pqrstuúùủũụưứừửữự" +
+                    "vwx" +
+                    "yýỳỷỹỵ";
+
+    private static final String VI_UPPER =
+            "AÁÀẢÃẠÂẤẦẨẪẬĂẮẰẲẴẶ" +
+                    "BCDĐ" +
+                    "EÉÈẺẼẸÊẾỀỂỄỆ" +
+                    "GHIÍÌỈĨỊ" +
+                    "KLMNOÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢ" +
+                    "PQRSTUÚÙỦŨỤƯỨỪỬỮỰ" +
+                    "VWX" +
+                    "YÝỲỶỸỴ";
+
+    private static final String DIGITS = "0123456789";
+
+    // process encrypt for text by affine cipher
+    public String encrypt(String plainText, String language, String keyAStr, String keyBStr) {
+
+        int a = normalizeA(keyAStr, language);
+        int m = gettmode(language);
+        int b = Math.floorMod(Integer.parseInt(keyBStr), m);
+
+
+
+        StringBuilder result = new StringBuilder();
+
+        for (char c : plainText.toCharArray()) {
+
+            if (isEnglishMode(language)) {
+                result.append(encryptForChar(c, EN_LOWER, EN_UPPER, a, b));
+            } else if (isVietnameseMode(language)) {
+                result.append(encryptForChar(c, VI_LOWER, VI_UPPER, a, b));
+            } else {
+                result.append(c);
+            }
+        }
+
+
+
+        return result.toString();
+    }
+    // process decrypt for text by affine cipher
+    public String decrypt(String text, String language, String keyaStr, String keybStr) {
+
+        int a = normalizeA(keyaStr, language);
+
+        int m = gettmode(language);
+        int b = Math.floorMod(Integer.parseInt(keybStr), m);
+
+
+
+        int inverseA = modInverse(a, m);
+
+        StringBuilder result = new StringBuilder();
+
+        for (char c : text.toCharArray()) {
+
+            if (isEnglishMode(language)) {
+                result.append(DecryptForChar(c, EN_LOWER, EN_UPPER, inverseA, b, m));
+            } else if (isVietnameseMode(language)) {
+                result.append(DecryptForChar(c, VI_LOWER, VI_UPPER, inverseA, b, m));
+            } else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+
+    // process for encrypt character by affine cipher
+    private char encryptForChar(char c, String lower, String upper, int a, int b) {
+
+        int m = lower.length();
+
+
+        if (Character.isDigit(c)) {
+            int idx = DIGITS.indexOf(c);
+            if (idx >= 0) {
+                int newIndex = (a * idx + b) % DIGITS.length();
+                return DIGITS.charAt(newIndex);
+            }
+            return c;
+        }
+
+        int idxLower = lower.indexOf(c);
+        if (idxLower >= 0) {
+            int newIndex = (a * idxLower + b) % m;
+            return lower.charAt(newIndex);
+        }
+
+
+        int idxUpper = upper.indexOf(c);
+        if (idxUpper >= 0) {
+            int newIndex = (a * idxUpper + b) % m;
+            return upper.charAt(newIndex);
+        }
+
+        return c;
+    }
+    // process for decrypt character by affine cipher
+    private char DecryptForChar(char c, String lower, String upper, int invA, int b, int m) {
+
+        if (Character.isDigit(c)) {
+            int idx = DIGITS.indexOf(c);
+            if (idx >= 0) {
+                int newIndex = mod(invA * (idx - b), DIGITS.length());
+                return DIGITS.charAt(newIndex);
+            }
+            return c;
+        }
+
+
+
+        int idxLower = lower.indexOf(c);
+        if (idxLower >= 0) {
+            int newIndex = mod(invA * (idxLower - b), m);
+            return lower.charAt(newIndex);
+        }
+
+        int idxUpper = upper.indexOf(c);
+        if (idxUpper >= 0) {
+            int newIndex = mod(invA * (idxUpper - b), m);
+            return upper.charAt(newIndex);
+        }
+
+        return c;
+    }
+
+    /**
+     * method for mod value in case of negative value
+     * @param value
+     * @param mod
+     * @return
+     */
+    private int mod(int value, int mod) {
+        int r = value % mod;
+        return (r < 0) ? r + mod : r;
+    }
+
+    private int modInverse(int a, int m) {
+        a = a % m;
+
+        for (int x = 1; x < m; x++) {
+            if ((a * x) % m == 1) {
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("a không có nghịch đảo modular!");
+    }
+
+//    public int normalizeA(String aStr, String language) {
+//        int m = gettmode(language);
+//        int a = Integer.parseInt(aStr);
+//        return Math.floorMod(a, m);
+//    }
+    public int normalizeA(String aStr, String language) {
+        int m = gettmode(language);
+        int a = Integer.parseInt(aStr);
+
+        a = Math.floorMod(a, m);
+
+        if (gcd(a, m) != 1) {
+            throw new IllegalArgumentException("Key A không hợp lệ!");
+        }
+
+        return a;
+    }
+
+
+    public String generateKeyA(String language) {
+        int m = gettmode(language);
+        Random rd = new Random();
+
+        int a;
+        do {
+            a = rd.nextInt(m - 1) + 1;
+        } while (gcd(a, m) != 1);
+
+        return String.valueOf(a);
+    }
+
+    public String generateKeyB(String language) {
+        int m = gettmode(language);
+        Random rd = new Random();
+        return String.valueOf(rd.nextInt(m));
+    }
+
+    private int gettmode(String language) {
+        return isVietnameseMode(language)
+                ? VI_LOWER.length()
+                : EN_LOWER.length();
+    }
+
+    private boolean isEnglishMode(String language) {
+        return language != null && language.equalsIgnoreCase("English");
+    }
+
+    private boolean isVietnameseMode(String language) {
+        return language != null && language.equalsIgnoreCase("Vietnamese");
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 
 }
